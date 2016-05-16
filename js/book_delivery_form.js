272ea823bf60305989d9a184cf716f2e519a9fe5
dungnd58 <ngoducdung95@gmail.com>
2016-05-16 18:13:22 +0700

    Add js files

---
+++ js/book_delivery_form.js
@@ -0,0 +1,1 @@
+var dServ = new DeliveryServiceForm();
---
+++ js/delivery_service_form.js
@@ -0,0 +1,44 @@
+function DeliveryServiceForm(){
+	this.requestID = 0;
+	this.uName = '';
+	this.uPhone = '';
+	this.dFromLocation = '';
+	this.dToLocation = '';
+	this.dTime = '';
+	this.dTypeOfCargo = '';
+	this.dSpecialRequirement = '';
+
+	this.createService = function(){};
+
+	this.validateInfo = function(){};
+
+	this.viewRequestList = function(){};
+
+	this.viewRequestDetails = function(){};
+
+	this.viewDeliveryService = function(){};
+
+	this.getRequestList = function(){};
+
+	this.getRequestByID = function(){};
+
+	this.confirmBid = function(){};
+
+	this.confirmDelete = function(){};
+
+	this.submit = function(){};
+
+	this.editRequestDetails = function(){};
+
+	this.deleteRequest = function(){};
+
+	this.searchAuto = function(){};
+
+	this.searchBy = function(){};
+
+	this.searchFilter = function(){};
+
+	this.uploadDeliveryRequest = function(){};
+
+	this.notificate = function(){};
+}
